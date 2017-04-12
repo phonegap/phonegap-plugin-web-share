29f6b79353a412b8c96ea337bf7c2ce3fef9a448
Simon MacDonald <simon.macdonald@gmail.com>
2017-04-12 10:57:12 -0400

    :sparkles: Implementing web share API

--- www/webshare.js
+++ www/webshare.js
@@ -4,29 +4,20 @@
 var exec = cordova.require('cordova/exec'),
     utils = cordova.require('cordova/utils');
 
-//var eddy = require('eddy-share');
-
-
-
 module.exports = function(sharedata) {
-
-	 var p = new Promise(function(resolve, reject) {
-
-	// 		//resolve if user chooses a target app and reject if any condition not met
-
-	// 		//sample scenario to validate tests
-
-			if(sharedata.hasOwnProperty('title') ||
-                sharedata.hasOwnProperty('text') ||
-                sharedata.hasOwnProperty('url')) {
-				resolve();
-			}
-			else {
-				reject();
-			}
-
-	 });
-
-
-	return p;
+    return new Promise(function(resolve, reject) {
+        var options = {
+            subject: sharedata.title,
+            message: sharedata.text,
+            url: sharedata.url
+        };
+		if(sharedata.hasOwnProperty('title') ||
+            sharedata.hasOwnProperty('text') ||
+            sharedata.hasOwnProperty('url')) {
+			window.plugins.socialsharing.shareWithOptions(options, resolve, reject);
+		}
+		else {
+			reject();
+		}
+	});
 };
